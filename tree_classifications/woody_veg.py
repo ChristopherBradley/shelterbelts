6043d148fba45b88e544af3656b544f4eb04acaa
Christopher Bradley <cb8590@gadi-login-07.gadi.nci.org.au>
2025-03-20 14:21:29 +1100

    Generated woodyveg tiles matching the imagery i

--- tree_classifications/woody_veg.py
+++ tree_classifications/woody_veg.py
@@ -2,62 +2,37 @@
 # Create 5km woody veg tiles to match the sentinel training data
 # -
 
-import rasterio
-import pandas as pd
-import numpy as np
-from rasterio.windows import Window
-from rasterio.transform import rowcol
-import pyproj
-import xarray as xr
+import os
+import glob
 import pickle
 import rioxarray as rxr
-from rasterio.enums import Resampling
 
-# Change directory to the repo root
-import os
-import sys
-if os.path.expanduser("~").startswith("/home/"):  # Running on Gadi
-    paddockTS_dir = os.path.join(os.path.expanduser("~"), "Projects/shelterbelts")
-elif os.path.basename(os.getcwd()) != "shelterbelts":
-    paddockTS_dir = os.path.dirname(os.getcwd())  # Running in a jupyter notebook 
-else:  # Already running locally from PaddockTS root
-    paddockTS_dir = os.getcwd()
-os.chdir(paddockTS_dir)
-sys.path.append(paddockTS_dir)
-print(paddockTS_dir)
-
-# Double checking the satellite imagery xarrays line up with the tiles. They almost do (EPSG:4326 vs EPSG:6933)
-filename = "data/41_80_147_34_ds2.pkl"
-with open(filename, 'rb') as file:
-    ds2 = pickle.load(file)
-filename = 'ds_41_80_147_34_ds2_red.tif'
-ds2['nbart_red'].rio.to_raster(filename)
+outdir = "/g/data/xe2/cb8590/shelterbelts/"
 
 # Load the woody veg tiff file
-filename = "data/Tas_WoodyVeg_201903_v2.2.tif"
+filename = os.path.join(outdir, "Tas_WoodyVeg_201903_v2.2.tif")
+ds = rxr.open_rasterio(filename)
 
-# Load the tile coordinates
-filename = "data/lidar_tiles_tasmania.csv"
+tiles = glob.glob("/g/data/xe2/cb8590/shelterbelts/*_ds2.pkl")
 
 # +
-# For each tile coordinate, slice the woody veg and save as a smaller tiff with a matching filename to the sentinel
+# %%time
+# For each tile, load the satellite imagery and use reproject_match to clip the woodyveg tif
+# (Would be more computationally efficient to not load the satellite imagery, but the code is simpler this way)
+
+for tile in tiles:    
+    # Load the satellite imagery for this tile
+    with open(tile, 'rb') as file:
+        ds2 = pickle.load(file)
+        
+    # reproject_match
+    ds_matched = ds.rio.reproject_match(ds2)
+    
+    # Save the cropped tif
+    stub = tile.replace(outdir,"").replace("_ds2.pkl","")
+    filename = os.path.join(outdir, f"{stub}_woodyveg_2019.tif")
+    ds_matched.rio.to_raster(filename)
+    print("Saved", filename)
 # -
 
-raster_path = "data/Tas_WoodyVeg_201903_v2.2.tif"
-csv_path = "data/lidar_tiles_tasmania.csv"
-output_dir = "data/woody_veg_tiles"
 
-df = pd.read_csv(csv_path)
-
-ds = rxr.open_rasterio(raster_path)
-
-# %%time
-# Make the woody veg EPSG match the satellite imagery imagery. 
-# The alternative it to specify EPSG:3577 when downloading the satellite imagery from DEA
-ds = ds.rio.reproject("EPSG:6933", resampling=Resampling.bilinear)
-# Took 2 mins 30 secs
-
-# %%time
-ds_matched = ds.rio.reproject_match(ds2)
-ds_matched.rio.to_raster(output_file)
-print(output_file)
