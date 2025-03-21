aa5ef3e62c6513f6310eaf6192d08bf7cc167acf
Christopher Bradley <cb8590@gadi-login-07.gadi.nci.org.au>
2025-03-21 14:39:25 +1100

    random_forest hyper paramter tuning

--- tree_classifications/5.random_forest.py
+++ tree_classifications/5.random_forest.py
@@ -13,49 +13,50 @@ pd.set_option('display.max_columns', 100)
 
 random_state = 0
 
-# From my testing, feather seems like the fastest and smallest filetype for a panda dataframe (better than parquet or csv, although csv more readable)
 # %%time
+# From my testing, feather seems like the fastest and smallest filetype for a panda dataframe (better than parquet or csv, although csv more readable)
 filename = "/g/data/xe2/cb8590/shelterbelts/woody_veg_preprocessed.feather"
 df = pd.read_feather(filename) 
 df.shape
 
 # Somehow I ended up with 3219 labels = 0.00393701. Need to look into if these should be 0 or NaN. 
-df['woody_veg'] = round(df['woody_veg'])
+# df = df[(df['woody_veg'] == 0) | (df['woody_veg'] == 1)]
+df['woody_veg'] = df['woody_veg'].round()
 
-# Start out by training on just 60k samples like in Stewart et al. 2025
-df_60k = df.iloc[:1000]
+sample_sizes = [1000, 10000, 60000, 100000, 200000]
 
-X = df_60k.drop(columns=['woody_veg', 'y', 'x', 'Unnamed: 0']) # input variables
-y = df_60k['woody_veg']  # target variable
-
-# Split the data into training and testing sets (70% train, 30% test)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=random_state)
+# %%time
+for sample_size in sample_sizes:
+    # Start out by training on just 60k samples like in Stewart et al. 2025
+    df_60k = df.sample(n=sample_size, random_state=random_state)
+    
+    X = df_60k.drop(columns=['woody_veg', 'y', 'x', 'Unnamed: 0']) # input variables
+    y = df_60k['woody_veg']  # target variable
+    
+    # Split the data into training and testing sets (70% train, 30% test)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=random_state)
+        
+    # Parameters same as Stewart et al. 2025
+    rf_classifier = RandomForestClassifier(n_estimators=100, min_samples_split=10, random_state=random_state)
+    
+    # Train the model
+    rf_classifier.fit(X_train, y_train)
+    
+    # Make predictions on the test set
+    y_pred = rf_classifier.predict(X_test)
+    
+    # Evaluate the model
+    accuracy = accuracy_score(y_test, y_pred)
+    print(f'sample_size: {sample_size}, Accuracy: {accuracy * 100:.2f}%' )
+    print(classification_report(y_test, y_pred))
 
 # +
-# %%time
-# Parameters same as Stewart et al. 2025
-rf_classifier = RandomForestClassifier(n_estimators=100, min_samples_split=20, min_samples_leaf=10, random_state=random_state)
-
-# Train the model
-rf_classifier.fit(X_train, y_train)
-
 # Took 3 secs to train 10k samples using 32GB RAM
 # Took 36 secs to train 60k samples using 32GB RAM
 
-# +
-# Make predictions on the test set
-y_pred = rf_classifier.predict(X_test)
-
-# Evaluate the model
-accuracy = accuracy_score(y_test, y_pred)
-print(f'Accuracy: {accuracy * 100:.2f}%')
-
-# Classification report for detailed evaluation metrics
-print("Classification Report:\n", classification_report(y_test, y_pred))
-
-# 94% accuracy with 60k total samples (42k training, 18k testing)
-# 94% accuracy with 10k samples, but slightly lower precision and recall for the 0's compared to 1's
-# 94% accuracy with 10k samples, but much lower precision and recall for the 0's compared to 1's
-# -
-
-
+# I should be expecting a validation accuracy of 0.93 based on Stewart et al. 2025. I'm getting 94% accuracy if I increase the sample size to 200k
+# Based on tuning min_samples_leaf [1,2,5,10,20,40,60], accuracy goes down slightly as min_samples_leaf is increased
+# Based on tuning min_samples_splits [1,2,5,6,7,8,9,10,12,14,16,18,20,40,60], accuracy goes up slightly to 5 and down slightly after 10
+# Based on tuning n_estimators [10, 50, 100, 200, 300], accuracy goes up slightly to 100 and down slightly after that. Also takes much longer
+# Based on tuning sample_size [10, 50, 100, 200], accuracy goes down as sample size is increased if not using randomly distributed values
+# Based on tuning sample_size [10, 50, 100, 200], accuracy goes up as sample size is increased if using randomly distributed values
