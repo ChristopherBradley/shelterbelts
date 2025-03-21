7443dcae0b25c970b1ee6c8d6135610db872ebb7
Christopher Bradley <chris2bradley@gmail.com>
2025-03-21 16:33:38 +1100

    Super simple worldcover download matching DAESim_preprocess args

--- shelterbelt_identification/worldcover.py
+++ shelterbelt_identification/worldcover.py
@@ -1,108 +1,46 @@
 # +
-# Downloading WorldCover using the same input parameters as the global canopy height
-
-# +
-# Following this example to download from the microsoft planetary computer
-# https://planetarycomputer.microsoft.com/dataset/esa-worldcover#Example-Notebook
-
-# +
-# # !pip install rich
+# Example code is here: https://planetarycomputer.microsoft.com/dataset/esa-worldcover#Example-Notebook
 # -
 
-import planetary_computer
+import os
+import odc.stac
 import pystac_client
+import planetary_computer
 
-
-bbox_of_interest = [33.984, 0.788, 34.902, 1.533]
-
-
-# +
-catalog = pystac_client.Client.open(
-    "https://planetarycomputer.microsoft.com/api/stac/v1",
-    modifier=planetary_computer.sign_inplace,
-)
-
-search = catalog.search(
-    collections=["esa-worldcover"],
-    bbox=bbox_of_interest,
-)
-
-items = list(search.get_items())
-items
-
-# +
-import rich.table
-
-# Assets
-t_assets = rich.table.Table("Key", "Value")
-for key, asset in items[0].assets.items():
-    t_assets.add_row(key, asset.title)
-t_assets
-# -
-
-# Metadata
-t_metadata = rich.table.Table("Key", "Value")
-for k, v in sorted(items[0].properties.items()):
-    t_metadata.add_row(k, str(v))
-t_metadata
-
-# +
-from IPython.display import Image
-
-Image(url=items[0].assets["rendered_preview"].href)
-
-# +
-class_list = items[0].assets["map"].extra_fields["classification:classes"]
-classmap = {
-    c["value"]: {"description": c["description"], "hex": c["color-hint"]}
-    for c in class_list
+world_cover_layers = {
+    "Tree cover": 10, # Green
+    "Shrubland": 20, # Orange
+    "Grassland": 30, # Yellow
+    "Cropland": 40, # pink
+    "Built-up": 50, # red
+    "Permanent water bodies": 80, # blue
 }
 
-t = rich.table.Table("Value", "Description", "Hex Color")
-for k, v in classmap.items():
-    t.add_row(str(k), v["description"], v["hex"])
-t
 
-# +
-import matplotlib.colors
+def worldcover(lat=-34.3890427, lon=148.469499, buffer=0.005, outdir=".", stub="Test"):
+    """Download worldcover data for the region of interest"""
+    bbox = [longitude - buffer, latitude - buffer, longitude + buffer, latitude + buffer]
+    catalog = pystac_client.Client.open(
+        "https://planetarycomputer.microsoft.com/api/stac/v1",
+        modifier=planetary_computer.sign_inplace,
+    )
+    search = catalog.search(
+        collections=["esa-worldcover"],
+        bbox=bbox_of_interest,
+    )
+    items = list(search.items())
+    items = [items[0]]
+    ds = odc.stac.load(items, crs="EPSG:4326", bbox=bbox_of_interest)
+    ds_map = ds.isel(time=0)['map']
 
-colors = ["#000000" for r in range(256)]
-for key, value in classmap.items():
-    colors[int(key)] = f"#{value['hex']}"
-cmap = matplotlib.colors.ListedColormap(colors)
+    filename = os.path.join(outdir, f"{stub}_worldcover.tif")
+    ds_map.rio.to_raster(filename)
+    print("Downloaded", filename)
+    
+    return ds_map
 
-# sequences needed for an informative colorbar
-values = [key for key in classmap]
-boundaries = [(values[i + 1] + values[i]) / 2 for i in range(len(values) - 1)]
-boundaries = [0] + boundaries + [255]
-ticks = [(boundaries[i + 1] + boundaries[i]) / 2 for i in range(len(boundaries) - 1)]
-tick_labels = [value["description"] for value in classmap.values()]
 
-# +
-import odc.stac
+if __name__ == '__main__':
+    worldcover()
 
-ds = odc.stac.load(items, crs="EPSG:4326", resolution=0.0001, bbox=bbox_of_interest)
-map_data = ds["map"].isel(time=-1).load()
-map_data
 
-# +
-from matplotlib import cm
-import matplotlib.pyplot as plt
-
-fig, ax = plt.subplots(figsize=(16, 14))
-normalizer = matplotlib.colors.Normalize(vmin=0, vmax=255)
-
-map_data.isel(latitude=slice(3000, 6000), longitude=slice(4000, 7000)).plot(
-    ax=ax, cmap=cmap, norm=normalizer
-)
-
-colorbar = fig.colorbar(
-    cm.ScalarMappable(norm=normalizer, cmap=cmap),
-    boundaries=boundaries,
-    values=values,
-    cax=fig.axes[1].axes,
-)
-colorbar.set_ticks(ticks, labels=tick_labels)
-
-ax.set_axis_off()
-ax.set_title("ESA WorldCover at Mount Elgin");
