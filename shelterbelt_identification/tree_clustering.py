77b7d7da26ec7d89d273a52cd0f52227c1f04579
Christopher Bradley <chris2bradley@gmail.com>
2025-03-24 21:09:31 +1100

    Planning tree clustering stats

--- shelterbelt_identification/tree_clustering.py
+++ shelterbelt_identification/tree_clustering.py
@@ -1,4 +1,8 @@
+# +
 # Group trees together and calculate statistics including: 
-# Number of shelterbelts w/ length, width, height, area, perimeter for each (and then mean and sd)
+# Number of shelterbelts w/ length, width, area, perimeter, height (min, mean, max) for each (and then mean and sd)
 # Area of sheltered and unsheltered crop & pasture by region and different thresholds
+# -
+
+
 
