d661b7fc332c178f1dd47e9dd1b0987d656f0093
Christopher Bradley <chris2bradley@gmail.com>
2025-04-02 14:28:18 +1100

    Aligned xarray plot & matplotlib plot by origin=lower

--- shelterbelt_identification/google_dynamic_world.py
+++ shelterbelt_identification/google_dynamic_world.py
@@ -6,12 +6,7 @@
 import ee
 import geemap
 import xarray as xr
-import os
-import rasterio
 import matplotlib.pyplot as plt
-import requests
-import zipfile
-from IPython.display import Image
 
 ee.Authenticate()
 ee.Initialize()
@@ -24,60 +19,12 @@ polygon_coords = [(bbox[0], bbox[1]), (bbox[0], bbox[3]), (bbox[2], bbox[3]), (b
 roi = ee.Geometry.Polygon([polygon_coords])
 bbox
 
-# +
-# From the DEM-H Data page, converted from javascript to python
-dataset = ee.Image('AU/GA/DEM_1SEC/v10/DEM-H');
-elevation = dataset.select('elevation');
-Image(url=elevation.getThumbURL({
-    'min': -10.0, 'max': 1300, 'dimensions': 512, 'region': roi,
-    'palette': ['006633', 'E5FFCC', '662A00', 'D8D8D8', 'F5F5F5']}))
-
-asset_url = elevation.getDownloadURL({
-    'scale': 30,
-    'crs': 'EPSG:4326',
-    'fileFormat': 'GeoTIFF',
-    'region': roi})
-print(asset_url)
-# -
-
-# Download the zip file
-response = requests.get(asset_url, stream=True)
-zip_path = 'asset.zip'
-with open(zip_path, 'wb') as file:
-    for chunk in response.iter_content(chunk_size=128):
-        file.write(chunk)
-
-# Unzip the file
-with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-    zip_ref.extractall('asset')
-
-# Read the TIFF file using rasterio
-tif_files = [f for f in os.listdir('asset') if f.endswith('.tif')]
-tif_path = os.path.join('asset', tif_files[0])
-with rasterio.open(tif_path) as src:
-    dem_data = src.read(1)  # Read the first band into a NumPy array
-
-# Display the NumPy array
-plt.imshow(dem_data, cmap='terrain')
-plt.colorbar(label='Elevation (m)')
-plt.title('DEM')
-plt.show()
+# Download the data into a numpy array using 
+dataset = ee.Image('AU/GA/DEM_1SEC/v10/DEM-H')
+array = geemap.ee_to_numpy(dataset, region=roi, bands=['elevation'], scale=30)
+array_2d = array[:,:,0]
+dem_xr = xr.DataArray(array_2d, dims=('y', 'x'), name='elevation')
 
 dem_xr.plot(cmap='terrain')
 
-plt.imshow(dem_xr.values)
-
-# +
-# Define the dataset and region of interest
-dataset = ee.Image('AU/GA/DEM_1SEC/v10/DEM-H')
-
-# Convert GEE image to NumPy
-array = geemap.ee_to_numpy(dataset, region=roi, bands=['elevation'], scale=30)
-# -
-
-array_2d = array[:,:,0]
-
-# Convert NumPy array to xarray
-dem_xr = xr.DataArray(array_2d, dims=('y', 'x'), name='elevation')
-
-
+plt.imshow(dem_xr.values, cmap='terrain', origin='lower')
