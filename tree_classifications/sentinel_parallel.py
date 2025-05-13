d28c7410b560e1c637ed3ed08973abd3ad511613
Christopher Bradley <cb8590@gadi-login-07.gadi.nci.org.au>
2025-05-13 12:14:50 +1000

    Looking at sentinel bbox region sizes

--- tree_classifications/sentinel_parallel.py
+++ tree_classifications/sentinel_parallel.py
@@ -18,6 +18,8 @@ import datacube
 import hdstats
 import pandas as pd
 import geopandas as gpd
+from shapely.ops import transform
+from pyproj import Transformer
 import rasterio
 
 from dea_tools.temporal import xr_phenology, temporal_statistics
@@ -74,9 +76,9 @@ def load_and_process_data(dc, query):
         )
     return ds
 
-def sentinel_download(tif, year, outdir, bounds, src_crs):
+# +
+def sentinel_download(stub, year, outdir, bounds, src_crs):
     
-    tif_id = '_'.join(tif.split('_')[:2])
     # print(f"Starting sentinel_download: {tif_id}, {year}\n")
 
     # Prep the DEA query
@@ -88,75 +90,33 @@ def sentinel_download(tif, year, outdir, bounds, src_crs):
     query = define_query_range(lat_range, lon_range, time_range, input_crs, output_crs)
 
     # Load the data
-    dc = datacube.Datacube(app=tif_id)
+    dc = datacube.Datacube(app=stub)
     ds = load_and_process_data(dc, query)
 
-    # Save the data
-    filename = os.path.join(outdir, f'{tif_id}_ds2_{year}.pkl')
+#     # Save the data
+#     # If I'm running across all of Australia then I probably don't have space to save every tile as a pickle fie
+#     filename = os.path.join(outdir, f'{tif_id}_ds2_{year}.pkl')
+#     with open(filename, 'wb') as handle:
+#         pickle.dump(ds, handle, protocol=pickle.HIGHEST_PROTOCOL)
+#     print(f"Saved {filename}", flush=True)
+    
+    return ds
 
-    with open(filename, 'wb') as handle:
-        pickle.dump(ds, handle, protocol=pickle.HIGHEST_PROTOCOL)
-    print(f"Saved {filename}", flush=True)
+
+# -
 
 def run_download(batch):
-    # Get each worker to download a bunch of sentinel tiles
+    # Get each worker to download a bunch of sentinel tiles based on Nick's tiff outlines
     for row in batch:
         tif, year, bounds, crs = row
         try:
             bbox = ast.literal_eval(bounds) # The bbox is saved as a string initially because I stored it in a csv file with pandas
             print(f"Downloading: {tif}_{year}", flush=True)
-            sentinel_download(tif, year, outdir, bbox, crs)
+            stub = '_'.join(tif.split('_')[:2])
+            sentinel_download(stub, year, outdir, bbox, crs)
         except Exception as e:
             print(f"Error in downloading: {tif}_{year}:", flush=True)
             traceback.print_exc(file=sys.stdout)
-
-    # try:
-    #     tif, year, bounds, crs = row
-    #     bbox = ast.literal_eval(bounds) # The bbox is saved as a string initially because I stored it in a csv file with pandas
-    #     print(f"Worker running: {tif}_{year}", flush=True)
-    #     sentinel_download(tif, year, outdir, bbox, crs)
-    # except Exception as e:
-    #     print(f"Error in worker {tif}_{year}:", flush=True)
-    #     traceback.print_exc(file=sys.stdout)
-    #     raise
-
-# +
-# # %%time
-# if __name__ == '__main__':
-
-# indir = '/g/data/xe2/cb8590/Nick_Aus_treecover_10m'
-# outdir = '/scratch/xe2/cb8590/Nick_sentinel'
-
-# csv_filename = '/g/data/xe2/cb8590/Nick_outlines/gdf_filename_maxyear.csv'
-# df_years = pd.read_csv(csv_filename, index_col='filename')
-# df_years_2017_2022 = df_years[df_years['year'] >= 2017]
-
-# df = df_years_2017_2022.sample(n=1000, random_state=0)
-# rows = list(df[['year']].itertuples(name=None))
-
-# # Pre-load the crs and bounds for each tif file
-# rows2 = []
-# import rasterio
-# for row in rows:
-#     tif, year = row
-#     filename = os.path.join(indir, tif)
-#     with rasterio.open(filename) as src:
-#         bounds = src.bounds
-#         crs = src.crs.to_string()
-#     rows2.append((tif, year, bounds, crs))
-
-# # I think the TIFF Read Error gets hidden in a jupyter notebook, so you only see it when doing qsub. But some workers seem to fail silently right now and don't download the pickle file.
-# with ProcessPoolExecutor(max_workers=len(rows)) as executor:
-#     print(f"Starting {len(rows)} workers")
-#     executor.map(run_download, rows2)
-# -
-
-indir = '/g/data/xe2/cb8590/Nick_Aus_treecover_10m'
-outdir = '/scratch/xe2/cb8590/Nick_sentinel'
-filename_bbox_year = "/g/data/xe2/cb8590/Nick_outlines/nick_bbox_year_crs.csv"
-filename_gdf_maxyear = '/g/data/xe2/cb8590/Nick_outlines/gdf_filename_maxyear.csv'
-outlines_dir = "/g/data/xe2/cb8590/Nick_outlines"
-
 
 # %%time
 def extract_bbox_year():
@@ -183,52 +143,87 @@ def extract_bbox_year():
     # Took 3 mins
 
 
-# +
-# Just trying out the gs_3117 tiff which failed in the qsub. It worked fine, confirming again this is a parallelisation error. 
-# rows = df[df['tif'] == 'g2_3117_binary_tree_cover_10m.tiff'][['tif', 'year', 'bbox', 'crs']].values.tolist()
+# Create rows for each of the 7k bbox's for tiffs Nick provided after 2017
+def prep_rows_Nick():   
+    indir = '/g/data/xe2/cb8590/Nick_Aus_treecover_10m'
+    outdir = '/scratch/xe2/cb8590/Nick_sentinel'
+    filename_bbox_year = "/g/data/xe2/cb8590/Nick_outlines/nick_bbox_year_crs.csv"
+    filename_gdf_maxyear = '/g/data/xe2/cb8590/Nick_outlines/gdf_filename_maxyear.csv'
+    outlines_dir = "/g/data/xe2/cb8590/Nick_outlines"
 
-# +
-# Load the tiff bboxs
-df = pd.read_csv(filename_bbox_year)
-df_2017_2022 = df[df['year'] >= 2017]
-print("Number of tiles between 2017-2022", len(df_2017_2022))
+    # Load the tiff bboxs
+    df = pd.read_csv(filename_bbox_year)
+    df_2017_2022 = df[df['year'] >= 2017]
+    print("Number of tiles between 2017-2022", len(df_2017_2022))
 
-# Find the tiles we have already downloaded
-sentinel_dir = "/scratch/xe2/cb8590/Nick_sentinel"
-sentinel_tiles = glob.glob(f'{sentinel_dir}/*')
-print("Number of sentinel tiles downloaded:", len(sentinel_tiles))
+    # Find the tiles we have already downloaded
+    sentinel_dir = "/scratch/xe2/cb8590/Nick_sentinel"
+    sentinel_tiles = glob.glob(f'{sentinel_dir}/*')
+    print("Number of sentinel tiles downloaded:", len(sentinel_tiles))
 
-# Find the tiles we haven't downloaded yet
-sentinel_tile_ids = ["_".join(sentinel_tile.split('/')[-1].split('_')[:2]) for sentinel_tile in sentinel_tiles]
-downloaded = [f"{tile_id}_binary_tree_cover_10m.tiff" for tile_id in sentinel_tile_ids]
-df_new = df_2017_2022[~df_2017_2022['tif'].isin(downloaded)]
-print("Number of new tiles to download:", len(df_new))
-# -
+    # Find the tiles we haven't downloaded yet
+    sentinel_tile_ids = ["_".join(sentinel_tile.split('/')[-1].split('_')[:2]) for sentinel_tile in sentinel_tiles]
+    downloaded = [f"{tile_id}_binary_tree_cover_10m.tiff" for tile_id in sentinel_tile_ids]
+    df_new = df_2017_2022[~df_2017_2022['tif'].isin(downloaded)]
+    print("Number of new tiles to download:", len(df_new))
 
-# Find tiles smaller than 1kmx1km
-too_smalls = []
-for i, row in df_2017_2022.iterrows():
-    bbox = ast.literal_eval(row['bbox']) 
-    left, bottom, right, top = bbox
-    width_m = right - left
-    height_m = top - bottom
-    too_small = width_m < 900 or height_m < 900
-    too_smalls.append(too_small)
-df_2017_2022['too_small'] = too_smalls
+    rows = df_new[['tif', 'year', 'bbox', 'crs']].values.tolist()
+    return rows
 
-# Find tiles that downloaded fine
-outdir = "/scratch/xe2/cb8590/Nick_csv2"
-csv_tiles = glob.glob(f'{outdir}/*')
-csv_ids = ["_".join(csv_tile.split('/')[-1].split('_')[:2]) for csv_tile in csv_tiles]
-csv_tree_ids = [f'{csv_id}_binary_tree_cover_10m.tiff' for csv_id in csv_ids]
-print("Number of merged_input_output csv files:", len(csv_tiles))
 
-# Find tiles that didn't download properly
-not_too_small = df_2017_2022[~df_2017_2022['too_small']]
-missing_tifs = not_too_small[~not_too_small['tif'].isin(csv_tree_ids)]
-df_new = missing_tifs
+outdir = '/scratch/xe2/cb8590/ka08_trees'
 
-rows = df_new[['tif', 'year', 'bbox', 'crs']].values.tolist()
+
+# Load the sentinel imagery tiles
+filename_sentinel_bboxs = "/g/data/xe2/cb8590/Nick_outlines/Sentinel-2-Shapefile-Index-master/sentinel_2_index_shapefile.shp"
+gdf = gpd.read_file(filename_sentinel_bboxs)
+
+test_tile_id = "55HFC"
+test_tile = gdf.loc[gdf['Name'] == test_tile_id, 'geometry'].values[0]
+
+polygon = test_tile
+
+test_tile_ids = "55HFC", "55HFD", "55HFE", "55HFB", "55HED", "55HDE", "55HCB"
+
+
+# Having a look at the size of some tiles
+for test_tile_id in test_tile_ids:
+    polygon = gdf.loc[gdf['Name'] == test_tile_id, 'geometry'].values[0]
+
+    # Get bounds of Sentinel polygon in degrees
+    minx, miny, maxx, maxy = polygon.bounds
+    width_deg = maxx - minx
+    height_deg = maxy - miny
+    print(f"Width in degrees: {width_deg}")
+    print(f"Height in degrees: {height_deg}")
+
+    # Determine UTM zone
+    centroid = polygon.centroid
+    lon, lat = centroid.x, centroid.y
+    zone_number = int((lon + 180) / 6) + 1
+    hemisphere = 'north' if lat >= 0 else 'south'
+    utm_crs = f"+proj=utm +zone={zone_number} +datum=WGS84 +units=m +no_defs"
+    if hemisphere == 'south':
+        utm_crs += " +south"
+
+    # Get bounds in metres
+    transformer = Transformer.from_crs("EPSG:4326", utm_crs, always_xy=True)
+    projected_polygon = transform(transformer.transform, polygon)
+    minx_m, miny_m, maxx_m, maxy_m = projected_polygon.bounds
+    width_m = maxx_m - minx_m
+    height_m = maxy_m - miny_m
+    print(f"Width in metres: {width_m:.2f}")
+    print(f"Height in metres: {height_m:.2f}")
+    print()
+    
+    # All the tiles are exactly 109.8km wide and 109.8km tall. 
+
+
+minx_m, miny_m, maxx_m, maxy_m
+
+
+
+
 
 workers = 1
 batch_size = math.ceil(len(rows) / workers)
