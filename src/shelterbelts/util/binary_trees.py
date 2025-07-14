c49bb2ec2aee02e55500ae9a72d457db0bb3294f
Christopher Bradley <cb8590@gadi-login-07.gadi.nci.org.au>
2025-07-14 16:34:14 +1000

    BUG: Using worldcover instead of abstract func

--- src/shelterbelts/util/binary_trees.py
+++ src/shelterbelts/util/binary_trees.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 import glob
 import xarray as xr
 import rioxarray as rxr
@@ -45,24 +46,72 @@ def worldcover_trees(filename, outdir):
     levels  = [2, 4, 8, 16, 32, 64]
     with rasterio.open(outpath, "r+") as src:          
         src.build_overviews(levels)
+
+
+def canopy_height_trees(filename, outdir):
+    """Convert a canopy height tif into a binary tree cover tif"""
+    da = rxr.open_rasterio(filename).isel(band=0).drop_vars('band')
+
+    # Anything taller than 1m
+    da_trees = (da >= 1)
+    da_trees = da_trees.astype('uint8')
+
+    # Coarsen from 1m to 10m horizontal resolution
+    da_trees = da_trees.rio.reproject(
+        da_trees.rio.crs,
+        resolution=10,                
+        resampling=rasterio.enums.Resampling.max  
+    )
     
+    stub = filename.split('/')[-1].split('.')[0]
+    
+    outpath = os.path.join(outdir, f"{stub}_woody_veg.tif")
+    tif_categorical(da_trees, outpath, cmap_woody_veg, tiled=True)
+    
+    # Add pyramid for faster viewing in zoomed out views QGIS
+    levels  = [2, 4, 8, 16, 32, 64]
+    with rasterio.open(outpath, "r+") as src:          
+        src.build_overviews(levels)
 
 
-def run_tifs(folder, func, outdir):
+# +
+funcs = {
+    "worldcover_trees": worldcover_trees,
+    "canopy_height_trees": canopy_height_trees
+}
+
+def run_tifs(folder, func_string, outdir, limit=None):
     """Run the function on every tif file in a folder"""
+    func = funcs[func_string]
     tif_files = glob.glob(os.path.join(folder, "*.tif*"))
+
+    if limit:
+        tif_files = tif_files[:limit]
+    
     for i, tif_file in enumerate(tif_files):
         print(f"{i+1}/{len(tif_files)}: Working on {tif_file}")
-        worldcover_trees(tif_file, outdir)
+        func(tif_file, outdir)
 
 
-# %%time
-if __name__ == '__main__':
-    folder = '/scratch/xe2/cb8590/Worldcover_Australia'
-    filename = os.path.join(folder,'ESA_WorldCover_10m_2021_v200_S36E147_Map.tif')
-    outdir = '/scratch/xe2/cb8590/worldcover_trees'
-    # worldcover_trees(filename, outdir)
+# -
+
+def parse_arguments():
+    """Parse command line arguments with default values."""
+    parser = argparse.ArgumentParser()
     
-    run_tifs(folder, worldcover_trees, outdir)
+    parser.add_argument('--folder', help='The folder containing all of the input tiffs')
+    parser.add_argument('--func_string', help="Either 'worldcover_trees' or 'canopy_height_trees'")
+    parser.add_argument('--outdir', default='.', help='The folder containing all of the output tiffs')
+    parser.add_argument('--limit', default=None)
+
+    return parser.parse_args()
+
+
+
+if __name__ == '__main__':
+    args = parse_arguments()
+    if args.limit:
+        args.limit = int(args.limit)
+    run_tifs(args.folder, args.func_string, args.outdir, args.limit)
 
 
