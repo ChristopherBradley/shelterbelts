fbc01e3cdce97cfcddadec2ea6eef02bccc3e2f6
Christopher Bradley <chris2bradley@gmail.com>
2025-07-28 16:00:47 +1000

    Finished class_metrics

--- src/shelterbelts/indices/shelter_metrics.py
+++ src/shelterbelts/indices/shelter_metrics.py
@@ -5,28 +5,120 @@ import numpy as np
 import pandas as pd
 import rioxarray as rxr
 
-from shelterbelts.indices.buffer_categories import buffer_categories_cmap, buffer_categories_labels
+from shelterbelts.indices.buffer_categories import buffer_categories_labels
+
+# Mapping for broader categories
+landcover_groups = {
+    10: "Trees",
+    30: "Grassland",
+    40: "Cropland",
+    50: "Built-up",
+    80: "Water"
+}
+def group_label(cat_id):
+    """Apply broader group categories to each pixel"""
+    return landcover_groups.get((cat_id // 10) * 10, 'Other')  # Flooring to the nearest 10
 
 
-def class_metrics(geometry, folder):
+# Need to make sure you've pip installed xlsxwriter for this to work
+def save_excel_sheets(dfs, filename):
+    """Save each dataframe in a separate sheet in an excel file, and resize columns nicely
+    dfs should be a dictionary of dataframes."""
+    with pd.ExcelWriter(filename, engine="xlsxwriter") as writer:
+    
+        for sheet_name, df in dfs.items():
+            df.to_excel(writer, sheet_name=sheet_name)
+            worksheet = writer.sheets[sheet_name]
+    
+            # Set the column widths to the longest item in that column
+            for i, col in enumerate(df.columns):
+                max_len = max(
+                    df[col].astype(str).map(len).max(),
+                    len(str(col))
+                )
+                worksheet.set_column(i + 1, i + 1, max_len)
+                
+            # Also set the index column width
+            max_idx_len = max(df.index.astype(str).map(len).max(), len(str(df.index.name or "")))
+            worksheet.set_column(0, 0, max_idx_len)
+    
+    print("Saved:", filename)
+
+
+def class_metrics(buffer_tif, outdir=".", stub="TEST", save_excel=True):
     """Calculate the percentage cover in each class.
     
     Parameters
     ----------
-        geometry: The region to calculate metrics
-        folder: The folder containing tif files with shelter categories for that region
-            
+        buffer_tif: A tif file where the integers represent the categories defined in 'buffer_category_labels'.
+
     Returns
     -------
-        dict_metrics: A dictionary with 4 dataframes: overall, landcover, trees, shelter
+        dfs: A dictionary with 4 dataframes: overall, landcover, trees, shelter
 
     Downloads
     ---------
         class_metrics.xlsx: An excel file with each dataframe in a separate tab.
 
     """
+    da = rxr.open_rasterio(buffer_tif).isel(band=0)
+
+    # Overall stats per category
+    counts = da.values.ravel()
+    counts = pd.Series(counts).value_counts().sort_index()
+    total_pixels = da.shape[0] * da.shape[1]
+    df_overall = pd.DataFrame({
+        'category_id': counts.index,
+        'label': [buffer_categories_labels.get(cat, 'Unknown') for cat in counts.index],
+        'pixel_count': counts.values,
+        'percentage': (counts.values / total_pixels) * 100
+    })
+    df_overall = df_overall.set_index('label')
+    
+    # Landcover groups
+    df_overall['landcover_group'] = df_overall['category_id'].apply(group_label)
+    df_landcover = df_overall.groupby('landcover_group')[['pixel_count', 'percentage']].sum()
+    df_landcover = df_landcover.sort_values(by='percentage', ascending=False)
+    df_landcover['percentage'] = df_landcover['percentage'].round(2)
+    
+    # Tree groups
+    df_trees = df_overall[df_overall['landcover_group'] == 'Trees'].copy()
+    total_tree_pixels = df_trees['pixel_count'].sum()
+    df_trees['percentage'] = (df_trees['pixel_count'] / total_tree_pixels) * 100
+    df_trees['percentage'] = df_trees['percentage'].round(2)
+    df_trees = df_trees.drop(columns=['category_id', 'landcover_group'])
+    df_trees = df_trees.sort_values(by='percentage', ascending=False)
+
+    # Shelter groups
+    df_production = df_overall[df_overall.index.str.contains("sheltered", case=False)].copy()
+    df_production['shelter_status'] = df_production.index.str.split().str[0] # First word is the shelter status
+    df_production['production_category'] = df_production.index.str.split().str[1]  # Last word is Grassland or Cropland
+    df_summary = df_sel.pivot_table(
+        index='production_category',
+        columns='shelter_status',
+        values='pixel_count',
+        aggfunc='sum',
+        fill_value=0
+    )
+    df_summary.loc['Total'] = df_summary.sum()
+    df_shelter = df_summary.div(df_summary.sum(axis=1), axis=0) * 100
+    df_shelter = df_shelter.round(2)
+
+    dfs = {
+        "Overall": df_overall,
+        "Landcover": df_landcover,
+        "Trees": df_trees,
+        "Shelter": df_shelter,
+    }
+
+    if save_excel:
+        filename = os.path.join(outdir, f"{stub}_class_metrics.xlsx")
+        save_excel_sheets(dfs, filename)
+
+    return dfs
 
 
+# +
 def patch_metrics(geometry, folder):
     """Calculate the length, width, height, direction, area, perimeter for each patch.
         Also calculates an overall mean and standard deviation for each attribute across all patches.
@@ -48,138 +140,28 @@ def patch_metrics(geometry, folder):
 
     """
 
+    
+# -
 
-
-# Read in the buffer categories
+# Inputs
 outdir = "../../../outdir/"
 stub = "g2_26729"
 buffer_tif = os.path.join(outdir, f"{stub}_buffer_categories.tif")
 
-
-da = rxr.open_rasterio(buffer_tif).isel(band=0)
-
-# +
-# Overall stats per category
-counts = da.values.ravel()
-counts = pd.Series(counts).value_counts().sort_index()
-total_pixels = da.shape[0] * da.shape[1]
-# total_pixels = np.isfinite(da.values).sum() # Could use this if I need to worry about NaN values
-
-# Construct a dataframe
-df_overall = pd.DataFrame({
-    'category_id': counts.index,
-    'label': [buffer_categories_labels.get(cat, 'Unknown') for cat in counts.index],
-    'pixel_count': counts.values,
-    'percentage': (counts.values / total_pixels) * 100
-})
-df_overall = df_overall.set_index('label')
-# df_overall = df_overall.sort_values(by='percentage', ascending=False)
-
-df_overall
+# Class metrics
+dfs = class_metrics(buffer_tif, outdir, stub)
 
 # +
-# Landcover groups
-landcover_groups = {
-    10: "Trees",
-    30: "Grassland",
-    40: "Cropland",
-    50: "Built-up",
-    80: "Water",
-}
-def group_label(cat_id):
-    """Apply broader group categories to each pixel"""
-    return landcover_groups.get((cat_id // 10) * 10, 'Other')  # Flooring to the nearest 10
-
-# Assign a group to each category
-df_overall['landcover_group'] = df_overall['category_id'].apply(group_label)
-
-df_landcover = df_overall.groupby('landcover_group')[['pixel_count', 'percentage']].sum()
-df_landcover = df_landcover.sort_values(by='percentage', ascending=False)
-df_landcover['percentage'] = df_landcover['percentage'].round(2)
-
-df_landcover
-
-# +
-# Tree groups
-df_trees = df_overall[df_overall['landcover_group'] == 'Trees'].copy()
-total_tree_pixels = df_trees['pixel_count'].sum()
-df_trees['percentage'] = (df_trees['pixel_count'] / total_tree_pixels) * 100
-df_trees['percentage'] = df_trees['percentage'].round(2)
-df_trees = df_trees.drop(columns=['category_id', 'landcover_group'])
-df_trees = df_trees.sort_values(by='percentage', ascending=False)
-
-df_trees
-
-# +
-# Shelter class stats
-df_production = df_overall[df_overall.index.str.contains("sheltered", case=False)].copy()
-df_production['shelter_status'] = df_production.index.str.split().str[0] # First word is the shelter status
-df_production['production_category'] = df_production.index.str.split().str[1]  # Last word is Grassland or Cropland
-
-df_summary = df_sel.pivot_table(
-    index='production_category',
-    columns='shelter_status',
-    values='pixel_count',
-    aggfunc='sum',
-    fill_value=0
-)
-df_summary.loc['Total'] = df_summary.sum()
-df_shelter = df_summary.div(df_summary.sum(axis=1), axis=0) * 100
-df_shelter = df_shelter.round(2)
-
-df_shelter
-# -
-
-with pd.ExcelWriter("class_metrics.xlsx", engine="xlsxwriter") as writer:
-    df_overall.to_excel(writer, sheet_name="Overall")
-    df_grouped.to_excel(writer, sheet_name="Landcover")
-    df_trees.to_excel(writer, sheet_name="Trees")
-    df_shelter.to_excel(writer, sheet_name="Shelter")
-
-
-# +
-dfs = {
-    "Overall": df_overall,
-    "Landcover": df_landcover,
-    "Trees": df_trees,
-    "Shelter": df_shelter,
-}
-filename = "class_metrics.xlsx"
-
-# Write each DataFrame to a different sheet
-with pd.ExcelWriter(filename, engine="xlsxwriter") as writer:
-
-    for sheet_name, df in dfs.items():
-        df.to_excel(writer, sheet_name=sheet_name)
-        worksheet = writer.sheets[sheet_name]
-
-        # Set the column widths to the longest item in that column
-        for i, col in enumerate(df.columns):
-            max_len = max(
-                df[col].astype(str).map(len).max(),
-                len(str(col))
-            )
-            worksheet.set_column(i + 1, i + 1, max_len)
-            
-        # Also set the index column width
-        max_idx_len = max(df.index.astype(str).map(len).max(), len(str(df.index.name or "")))
-        worksheet.set_column(0, 0, max_idx_len)
-
-# -
-
-
-
-# +
-# Do the class metrics
-# -
-
-
+# Patch metrics
 
 # +
 # Re-read masters thesis & R package
-# -
 
+# +
 # Reuse the scipy label function, and do more with each patch. MVP is just to have at least some stats per patch.
 
+# +
+# Create a map with labelled shelterbelts to go along with the excel file
+# -
 
 
