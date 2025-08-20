65ccf8730f48b3fa25a092eb1500c8b4229bd53f
Christopher Bradley <cb8590@gadi-login-07.gadi.nci.org.au>
2025-08-20 16:11:35 +1000

    Saving preprocessed data and trained model to file

--- src/shelterbelts/classifications/convolutional_network.py
+++ src/shelterbelts/classifications/convolutional_network.py
@@ -13,6 +13,8 @@ from tensorflow.keras import layers, Model
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 def monthly_mosaic(sentinel_file, tree_file=None, clip_percentile=(2, 98)):
@@ -113,18 +115,13 @@ def extract_patches(X, y, patch_size=64, stride=32):
 
 def preprocess_tile(sentinel_file, tree_file, patch_size=64, stride=32, clip_percentile=(2, 98)):
     """Load, normalise, and reshape the inputs and outputs for a single tile training the CNN"""
-    # print(sentinel_file)
-    # print(tree_file)
     monthly_tile, ds_tree = monthly_mosaic(sentinel_file, tree_file, clip_percentile)
 
-    # print("Extracting patches")
     # I want to add the option for tree_file to be None, so that I can use the same function when applying the model to unseen data
-    # print("mosaic_output_shapes", monthly_tile.shape, ds_tree.shape)
     X_p, y_p = extract_patches(monthly_tile, ds_tree.values, patch_size, stride)
     
     # Collapse the time & tile dimensions into a single channel dimension (removes temporal sequence information, but retains temporal variation)
     if len(X_p) > 0:
-        # print("Reshaping: ", X_p.shape, y_p.shape)
         X_p = X_p.reshape(
             X_p.shape[0],
             X_p.shape[1], 
@@ -133,29 +130,29 @@ def preprocess_tile(sentinel_file, tree_file, patch_size=64, stride=32, clip_per
         )
     return X_p, y_p
 
-def prep_tiles(sentinel_folder, tree_folder, patch_size=64, stride=32, clip_percentile=(2, 98), limit=None):
+def prep_tiles(sentinel_folder, tree_folder, outdir=".", stub="TEST", patch_size=64, stride=32, clip_percentile=(2, 98), limit=None):
     """Use a bunch of tiles to create the training and testing arrays for the CNN"""
 
     sentinel_files = sorted(glob.glob(os.path.join(sentinel_folder, "*.pkl")))
 
     # Initially just trying out 10 tiles as input, around canberra with a good distribution of trees and no trees
     interesting_tile_ids = [
-    "g2_017_",
-    "g2_019_",
-    "g2_021_",
-    "g2_21361_",
-    "g2_23939_",
-    "g2_23938_",
-    "g2_09_",
-    "g2_2835_",
-    "g2_25560_",
-    "g2_24903_"
+        "g2_017_",
+        "g2_019_",
+        "g2_021_",
+        "g2_21361_",
+        "g2_23939_",
+        "g2_23938_",
+        "g2_09_",
+        "g2_2835_",
+        "g2_25560_",
+        "g2_24903_"
     ]
     sentinel_files = [filename for filename in sentinel_files if any(tile_id in filename for tile_id in interesting_tile_ids)]
     
     sentinel_files = sentinel_files[:limit]
     
-    print("Number of tiles to use as input", len(sentinel_files))
+    print("Number of tiles to use as input:", len(sentinel_files))
 
     sentinel_tile_ids = ["_".join(sentinel_tile.split('/')[-1].split('_')[:2]) for sentinel_tile in sentinel_files]
     tree_files = [os.path.join(tree_folder, f'{tile_id}_binary_tree_cover_10m.tiff') for tile_id in sentinel_tile_ids]
@@ -170,13 +167,20 @@ def prep_tiles(sentinel_folder, tree_folder, patch_size=64, stride=32, clip_perc
 
     X_p = np.concatenate(all_X, axis=0)
     y_p = np.concatenate(all_y, axis=0)
-    print("Final dataset:", X_p.shape, y_p.shape)
 
-    # Should save this to file by adding an outdir and stub as inputs to this function
-
+    filename = os.path.join(outdir, f'{stub}_preprocessed.npz')
+    np.savez_compressed(filename, X=X_p, y=y_p)
+    print(f"Saved: {filename}")
+    
     return X_p, y_p
 
-
+def load_preprocessed_npz(outdir, stub):
+    filename = os.path.join(outdir, f'{stub}_preprocessed.npz')
+    data = np.load(filename)
+    print("Loaded:", filename)
+    X, y = data['X'], data['y']
+    return X, y
+    
 def conv_block(x, f):
     x = layers.Conv2D(f, 3, padding="same", activation="relu")(x)
     x = layers.Conv2D(f, 3, padding="same", activation="relu")(x)
@@ -192,7 +196,31 @@ def tiny_unet(input_shape):
     outputs = layers.Conv2D(1,1,activation="sigmoid")(c4)
     return Model(inputs, outputs)
 
-def train_model(X, y, test_size=0.2, random_state=1, batch_size=8, epochs=10):
+
+history_mapping = {'CategoricalAccuracy': 'Training Accuracy', 'val_CategoricalAccuracy': 'Testing Accuracy', 'loss': "Training Loss", 'val_loss': "Testing Loss"}
+def loss_plots(history, history_mapping, outdir=".", stub="TEST"):
+    
+    history_df = pd.DataFrame.from_dict(history.history)
+    history_df = history_df.rename(columns = history_mapping)
+    
+    fig, axes = plt.subplots(1, 2, figsize=(12,6))
+    
+    sns.lineplot(ax=axes[0], data=history_df[['Training Accuracy', 'Testing Accuracy']])
+    axes[0].set_title("Model Accuracy")
+    axes[0].set_xlabel("Epoch")
+    axes[0].set_ylabel("Accuracy")
+    
+    sns.lineplot(ax=axes[1], data=history_df[['Training Loss', 'Testing Loss']])
+    axes[1].set_title("Model Loss")
+    axes[1].set_xlabel("Epoch")
+    axes[1].set_ylabel("Loss")
+    
+    plt.tight_layout()
+    filename = os.path.join(outdir, f'{stub}_training_plots.png')
+    plt.savefig(filename)
+
+    
+def train_model(X, y, outdir=".", stub="TEST", test_size=0.2, random_state=1, batch_size=8, epochs=10):
 
     X_train, X_val, y_train, y_val = train_test_split(
         X, y, test_size=test_size, random_state=random_state
@@ -201,7 +229,7 @@ def train_model(X, y, test_size=0.2, random_state=1, batch_size=8, epochs=10):
     model = tiny_unet(input_shape=X_train.shape[1:])
     model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
 
-    model.fit(
+    history = model.fit(
         X_train, y_train,
         validation_data=(X_val, y_val),
         batch_size=batch_size,
@@ -209,21 +237,26 @@ def train_model(X, y, test_size=0.2, random_state=1, batch_size=8, epochs=10):
         callbacks=[tf.keras.callbacks.EarlyStopping(patience=3, restore_best_weights=True)]
     )
     
-    # Should save the model with outdir and stub as inputs
+    # Save the model
+    filename = os.path.join(outdir, f'cnn_{stub}.keras')
+    model.save(filename)
+    print("Saved", filename)
 
+    history_mapping = {'accuracy': 'Training Accuracy', 'val_accuracy': 'Testing Accuracy', 'loss': "Training Loss", 'val_loss': "Testing Loss"}
+    loss_plots(history, history_mapping, outdir, stub)
+    
+    # Create a classification report
     y_pred_prob = model.predict(X_val, batch_size=1)
     y_pred = (y_pred_prob > 0.5).astype(np.uint8)
 
     y_val_flat = y_val.flatten()
     y_pred_flat = y_pred.flatten()
 
-    print("\nClassification report (pixelwise):")
     print(classification_report(y_val_flat, y_pred_flat, digits=4))
-
-    # Should save the classification report and training png like with the MLP
+    
+    # Should save this classification report to file too
 
     return model
-
 
 
 # -
@@ -234,8 +267,32 @@ if __name__ == '__main__':
     sentinel_folder = '/scratch/xe2/cb8590/Nick_sentinel'
     tree_folder = '/g/data/xe2/cb8590/Nick_Aus_treecover_10m'
     limit = 10
+    
+    outdir = '/scratch/xe2/cb8590/tmp'
+    stub = 'CNN_TEST'
 
-    X, y = prep_tiles(sentinel_folder, tree_folder, limit=limit)
+    # X, y = prep_tiles(sentinel_folder, tree_folder, outdir, stub, limit=limit)
+    X, y = load_preprocessed_npz(outdir, stub)   # Use this if you've already preprocessed the data previously
 
-    model = train_model(X, y)
+    model = train_model(X, y, outdir, stub)
 
+
+# +
+# test_size = 0.2
+# random_state = 1
+# batch_size = 16
+# epochs=10
+# X_train, X_val, y_train, y_val = train_test_split(
+#     X, y, test_size=test_size, random_state=random_state
+# )
+
+# model = tiny_unet(input_shape=X_train.shape[1:])
+# model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+
+# history = model.fit(
+#     X_train, y_train,
+#     validation_data=(X_val, y_val),
+#     batch_size=batch_size,
+#     epochs=epochs,
+#     callbacks=[tf.keras.callbacks.EarlyStopping(patience=3, restore_best_weights=True)]
+# )
