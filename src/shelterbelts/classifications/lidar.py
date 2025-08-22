62080f7f04e29b3019201a870581cf096e40810d
Christopher Bradley <chris2bradley@gmail.com>
2025-08-22 11:43:38 +1000

    pdal creating 10m raster from scratch
    i.e. without using the pre-classified categories,
    so generating a DEM, DSM, CHM, and the reprojecting.

--- src/shelterbelts/classifications/lidar.py
+++ src/shelterbelts/classifications/lidar.py
@@ -1,14 +1,11 @@
 # +
-# Did a conda install of pdal
+# # !conda install -c conda-forge pdal python-pdal rasterio
 # -
 
 import pdal, json
 
-# +
 filename = '/Users/christopherbradley/Documents/PHD/Data/ESDALE/NSW_LiDAR_2018_80cm/Point Clouds/AHD/Brindabella201802-LID2-C3-AHD_6746112_55_0002_0002.laz'
 
-
-# -
 
 # Using the pre-classified values to create a 10m tree raster
 pipeline = {
@@ -25,123 +22,9 @@ pipeline = {
 p = pdal.Pipeline(json.dumps(pipeline))
 p.execute()
 
-
-
-
-# I also tried out laspy, but had issues converting from the numpy array to a rioxarray or tif because I had to manually create the latitude and longitude coordinates
-# laspy doesn't seem as powerful as pdal anyway (in terms of tree segmentation etc.)
-las = laspy.read(filename)
-z = las.z 
-tree_code = 5  # Classification code for trees > 2m
-tree_points = np.isin(las.classification, [tree_code])
-
-
-
-
-
-# +
-# ground classification & height normalization
-# %%time
-pipeline_json = {
-    "pipeline": [
-        filename,
-        {"type": "filters.smrf"},
-        {"type": "filters.hag_nn"},
-        "out_normalized.laz"
-    ]
-}
-pipeline = pdal.Pipeline(json.dumps(pipeline_json))
-pipeline.execute()
-
-# Took 26 secs
-# +
-# This just created the dem.tif, but the rest of the pipeline failed
-
-# %%time
-import json
-import pdal
-import rasterio
-import numpy as np
-
-filename = "/Users/christopherbradley/Documents/PHD/Data/ESDALE/NSW_LiDAR_2018_80cm/Point Clouds/AHD/Brindabella201802-LID2-C3-AHD_6746112_55_0002_0002.laz"
-
-# Outputs (GeoTIFFs)
-dem_tif = "dem.tif"       # ground surface
-dsm_tif = "dsm.tif"       # canopy/first-return surface
-chm_tif = "chm.tif"       # canopy height model (DSM - DEM)
-tree_tif = "tree_binary.tif"
-resolution = 1.0          # metres per pixel
-tree_height_thresh = 2.0  # metres (tweak as needed)
-
-# PDAL pipeline:
-# - SMRF: classify ground
-# - HAG:  HeightAboveGround
-# - DEM:  min(Z) of ground-only points
-# - DSM:  max(Z) of first returns
-# - CHM:  max(HeightAboveGround)
-pipeline_json = {
-    "pipeline": [
-        {"type": "readers.las", "filename": filename, "tag": "in"},
-
-        {"type": "filters.smrf", "inputs": "in", "tag": "smrf"},
-        {"type": "filters.hag_nn", "inputs": "smrf", "tag": "hag"},
-
-        # DEM from ground (Class 2)
-        {"type": "filters.range", "inputs": "smrf", "limits": "Classification[2:2]", "tag": "ground"},
-        {"type": "writers.gdal",
-         "inputs": "ground",
-         "filename": dem_tif,
-         "resolution": resolution,
-         "gdaldriver": "GTiff",
-         "output_type": "min",
-         "nodata": -9999},
-
-        # DSM from first returns
-        {"type": "filters.range", "inputs": "in", "limits": "ReturnNumber[1:1]", "tag": "first"},
-        {"type": "writers.gdal",
-         "inputs": "first",
-         "filename": dsm_tif,
-         "resolution": resolution,
-         "gdaldriver": "GTiff",
-         "output_type": "max",
-         "nodata": -9999},
-
-        # CHM = max(HeightAboveGround)
-        {"type": "writers.gdal",
-         "inputs": "hag",
-         "filename": chm_tif,
-         "resolution": resolution,
-         "gdaldriver": "GTiff",
-         "dimension": "HeightAboveGround",
-         "output_type": "max",
-         "nodata": -9999}
-    ]
-}
-
-p = pdal.Pipeline(json.dumps(pipeline_json))
-p.execute()
-
-# Binary tree raster from CHM (> threshold)
-with rasterio.open(chm_tif) as src:
-    chm = src.read(1)
-    prof = src.profile.copy()
-
-tree = (chm > tree_height_thresh).astype(np.uint8)
-prof.update(dtype=rasterio.uint8, nodata=0)
-
-with rasterio.open(tree_tif, "w", **prof) as dst:
-    dst.write(tree, 1)
-
-print("Wrote:", dem_tif, dsm_tif, chm_tif, tree_tif)
-
-
 # +
 # %%time
-import json
-import pdal
-
-filename = "/Users/christopherbradley/Documents/PHD/Data/ESDALE/NSW_LiDAR_2018_80cm/Point Clouds/AHD/Brindabella201802-LID2-C3-AHD_6746112_55_0002_0002.laz"
-
+# Doing my own classifications 
 dem_tif = "dem.tif"
 dsm_tif = "dsm.tif"
 chm_tif = "chm.tif"
@@ -200,8 +83,34 @@ chm_json = {
 p_chm = pdal.Pipeline(json.dumps(chm_json))
 p_chm.execute()
 
+
+# +
+import rioxarray
+import numpy as np
+from rasterio.enums import Resampling
+
+tree_height_thresh = 2
+tree_tif = "tree_binary_10m.tif"
+
+# Open CHM
+chm = rioxarray.open_rasterio("chm.tif")
+
+# Reproject to 10 m resolution, using max aggregation
+chm_10m = chm.rio.reproject(
+    chm.rio.crs,
+    resolution=10,
+    resampling=Resampling.max
+)
+
+# Apply threshold
+tree = (chm_10m > tree_height_thresh).astype(np.uint8)
+
+# Ensure nodata = 0
+tree = tree.rio.write_nodata(0)
+
+# Save to GeoTIFF
+tree.rio.to_raster(tree_tif, compress="LZW")
+
 # -
 
-
-
-
+print("Wrote:", dem_tif, dsm_tif, chm_tif, tree_tif)
