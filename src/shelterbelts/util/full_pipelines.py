f9f05d4b039e9575ceffb388403cf5356461e273
Christopher Bradley <cb8590@gadi-login-07.gadi.nci.org.au>
2025-09-05 15:30:34 +1000

    Random Forest matching neural network

--- src/shelterbelts/util/full_pipelines.py
+++ src/shelterbelts/util/full_pipelines.py
@@ -4,6 +4,21 @@ import os
 import geopandas as gpd
 import rasterio
 
+# # Change directory to this repo - this should work on gadi or locally via python or jupyter.
+import os, sys
+repo_name = "shelterbelts"
+if os.path.expanduser("~").startswith("/home/"):  # Running on Gadi
+    repo_dir = os.path.join(os.path.expanduser("~"), f"Projects/{repo_name}")
+elif os.path.basename(os.getcwd()) != repo_name:  # Running in a jupyter notebook 
+    repo_dir = os.path.dirname(os.getcwd())       
+else:                                             # Already running from root of this repo. 
+    repo_dir = os.getcwd()
+src_dir = os.path.join(repo_dir, 'src')
+os.chdir(src_dir)
+sys.path.append(src_dir)
+# print(src_dir)
+
+
 from shelterbelts.classifications.binary_trees import worldcover_trees, canopy_height_trees
 from shelterbelts.apis.worldcover import worldcover_bbox
 from shelterbelts.apis.hydrolines import hydrolines
@@ -14,6 +29,7 @@ from shelterbelts.indices.shelter_categories import shelter_categories
 from shelterbelts.indices.cover_categories import cover_categories
 from shelterbelts.indices.buffer_categories import buffer_categories
 from shelterbelts.indices.shelter_metrics import class_metrics, patch_metrics
+
 
 
 # +
