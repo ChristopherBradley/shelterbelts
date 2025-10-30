cb2096dcd4fe114df0e7242521a2015052c4cc5b
Christopher Bradley <cb8590@gadi-login-07.gadi.nci.org.au>
2025-10-30 12:58:10 +1100

    expand_tifs helper function to run on a folder of subfolders Please enter the commit message for your changes. Lines starting

--- src/shelterbelts/indices/expand_tifs.py
+++ src/shelterbelts/indices/expand_tifs.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import glob
 
 import rioxarray as rxr
 from shapely.geometry import box
@@ -9,7 +10,7 @@ from shelterbelts.classifications.bounding_boxes import bounding_boxes
 from shelterbelts.apis.canopy_height import merge_tiles_bbox, merged_ds
 
 
-def expand_tif(filename, folder, outdir, tmpdir='/scratch/xe2/cb8590/tmp', num_pixels=20, pixel_size=10):
+def expand_tif(filename, folder_merged, outdir, tmpdir='/scratch/xe2/cb8590/tmp', num_pixels=20, pixel_size=10):
     """Expand the tif by a certain number of pixels, to avoid edge effects when running indices at scale
     
     Parameters
@@ -39,7 +40,7 @@ def expand_tif(filename, folder, outdir, tmpdir='/scratch/xe2/cb8590/tmp', num_p
     if not os.path.exists(gpkg):
         bounding_boxes(merged_folder, crs='EPSG:3857')
     stub = Path(filename).stem
-    mosaic, out_meta = merge_tiles_bbox(expanded_bounds, tmpdir, stub, folder, gpkg, 'filename', verbose=False)
+    mosaic, out_meta = merge_tiles_bbox(expanded_bounds, tmpdir, stub, folder_merged, gpkg, 'filename', verbose=False)
     ds_expanded = merged_ds(mosaic, out_meta, 'expanded')
 
     outpath = os.path.join(outdir, f'{stub}_expanded{num_pixels}.tif')
@@ -49,56 +50,44 @@ def expand_tif(filename, folder, outdir, tmpdir='/scratch/xe2/cb8590/tmp', num_p
     return ds_expanded
 
 
+def expand_tifs(folder_to_expand, folder_merged, outdir, suffix='', non_suffixes=[''], non_contains=[''], limit=None):
+    """Run expand_tif on all subfolders in folder_to_expand, preserving the folder structure when writing to outdir"""
+    folders = glob.glob(f'{folder_to_expand}/*')
+
+    # I should have better folder management so I don't need to jump through all these hurdles
+    folders = [f for f in folders if f.endswith(suffix) 
+             and os.path.isdir(f)
+             and not any(f.endswith(non_suffix) for non_suffix in non_suffixes)
+             and not any(non_contain in f for non_contain in non_contains)
+            ]
+
+    if limit:
+        folders = folders[:limit]
+    for folder in folders:
+        filenames = glob.glob(f'{folder}/*')
+        sub_outdir = os.path.join(outdir, Path(folder).stem)
+        os.makedirs(sub_outdir, exist_ok=True)
+
+        if limit:
+            filenames = filenames[:limit]
+        for filename in filenames:
+            expand_tif(filename, folder_merged, sub_outdir)
+
+
 filename = '/scratch/xe2/cb8590/barra_trees_s4_2024/subfolders/lat_34_lon_148/34_93-148_90_y2024_predicted.tif'
-folder = '/scratch/xe2/cb8590/barra_trees_s4_2024/subfolders/merged_predicted'
+folder_merged = '/scratch/xe2/cb8590/barra_trees_s4_2024/subfolders/merged_predicted'
+outdir = '/scratch/xe2/cb8590/tmp'
+ds = expand_tif(filename, folder_merged, outdir)
 
-ds = expand_tif(filename, folder, '/scratch/xe2/cb8590/tmp')
+folder_to_expand = '/scratch/xe2/cb8590/barra_trees_s4_2024/subfolders'
+folder_merged = '/scratch/xe2/cb8590/barra_trees_s4_2024/subfolders/merged_predicted'
+outdir = '/scratch/xe2/cb8590/barra_trees_s4_2024/expanded'
 
-
-
-Path('/scratch/xe2/cb8590/barra_trees_s4_2024/subfolders/lat_34_lon_148/34_93-148_90_y2024_predicted.tif').stem
-
-
-filename = '/scratch/xe2/cb8590/barra_trees_s4_2024/subfolders/lat_34_lon_148/34_93-148_90_y2024_predicted.tif'
-
-da = rxr.open_rasterio(filename).isel(band=0).drop_vars('band')
-
-da
-
-bounds = da.rio.bounds()
-
-# +
-buffer = 100  # 10m x 10 pixels
-minx, miny, maxx, maxy = bounds
-expanded_bounds = (minx - buffer, miny - buffer, maxx + buffer, maxy + buffer)
-
-# Create a shapely box (polygon)
-geom = box(*expanded_bounds)
-
-# Save as gpkg
-gpd.GeoDataFrame({'geometry': [geom]}, crs='EPSG:3857').to_file('/scratch/xe2/cb8590/tmp/expanded_bounds.gpkg')
-
-# -
-
-
-
-merged_folder = '/scratch/xe2/cb8590/barra_trees_s4_2024/subfolders/merged_predicted'
-
-f'{Path(merged_folder).parent.stem}_{Path(merged_folder).stem}_footprints.gpkg'
-
-gpkg = 'subfolders_merged_predicted_footprints.gpkg'
+suffix=''
+non_suffixes=['_confidence50', '_confidence50_fixedlabels', '_corebugfix', '.tif']
+non_contains = ['linear_tifs', 'merged_predicted']
 
 # %%time
-gdf = bounding_boxes(merged_folder, crs='EPSG:3857')
+expand_tifs(folder_to_expand, folder_merged, outdir, suffix, non_suffixes, non_contains)
 
-gdf = gpd.read_file('/scratch/xe2/cb8590/barra_trees_s4_2024/subfolders/merged_predicted/subfolders_merged_predicted_footprints.gpkg')
 
-# +
-
-unique_stub = 'TEST'  # Needs to be unique to this tile, or you get merge errors
-# -
-
-mosaic, out_meta = merge_tiles_bbox(expanded_bounds, tmpdir, unique_stub, merged_folder, gpkg, 'filename', verbose=False)     # Need to include the DATA... in the stub so we don't get rasterio merge conflicts
-ds_expanded = merged_ds(mosaic, out_meta, 'expanded')
-
-ds_expanded['expanded'].rio.to_raster('/scratch/xe2/cb8590/tmp/expanded.tif')
