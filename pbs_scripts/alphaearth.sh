939bd80a9f390485a89f5aa104daa06e0d5569bc
Christopher Bradley <chris2bradley@gmail.com>
2025-11-05 15:56:09 +1100

    Removed obsolete pbs and sh files

--- pbs_scripts/alphaearth.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-folder="/scratch/xe2/cb8590/Nick_Aus_treecover_10m"
-
-for subfolder in "$folder"/subfolder_*; do
-    echo "Submitting job for $subfolder"
-    qsub -v subfolder="$subfolder" alphaearth.pbs
-done
-
--- pbs_scripts/prepend.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Prepend the folder name to each of the files
-for f in /scratch/xe2/cb8590/lidar/*/uint8_percentcover_res10_height2m/uint8_percentcover_res10_height2m_footprints.gpkg; do
-    folder=$(basename "$(dirname "$(dirname "$f")")")   # gets the * folder, e.g. DATA_12345
-    dir=$(dirname "$f")                                # parent directory
-    base=$(basename "$f")                              # original filename
-    mv "$f" "$dir/${folder}_${base}"
-done
