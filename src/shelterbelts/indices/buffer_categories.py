0278b3fe77d6470ddefa07d8b7976bab665a5056
Christopher Bradley <chris2bradley@gmail.com>
2026-02-03 17:00:12 +1100

    Buffer sphinx visuals looking good

--- src/shelterbelts/indices/buffer_categories.py
+++ src/shelterbelts/indices/buffer_categories.py
@@ -69,10 +69,40 @@ def buffer_categories(cover_data, gullies_data, ridges_data=None, roads_data=Non
     
     >>> from shelterbelts.utils import get_filename
     >>> cover_file = get_filename('g2_26729_cover_categories.tif')
-    >>> gullies_file = get_filename('g2_26729_gullies.tif')
+    >>> gullies_file = get_filename('g2_26729_DEM-S_gullies.tif')
     >>> ds = buffer_categories(cover_file, gullies_file, outdir='/tmp', plot=False, savetif=False)
     >>> 'buffer_categories' in set(ds.data_vars)
     True
+    
+    Here's how different parameters affect the buffer categorization:
+    
+    .. plot::
+
+        from shelterbelts.indices import buffer_categories, buffer_categories_cmap, buffer_categories_labels
+        from shelterbelts.utils import get_filename, visualise_categories_sidebyside
+
+        cover_file = get_filename('g2_26729_cover_categories.tif')
+        gullies_file = get_filename('g2_26729_DEM-S_gullies.tif')
+        ridges_file = get_filename('g2_26729_DEM-S_ridges.tif')
+        roads_file = get_filename('g2_26729_roads.tif')
+        
+        # buffer_width: 1 vs 5 pixels (gullies only)
+        ds1 = buffer_categories(cover_file, gullies_file, outdir='/tmp', stub='buf1', plot=False, savetif=False, buffer_width=1)
+        ds2 = buffer_categories(cover_file, gullies_file, outdir='/tmp', stub='buf5', plot=False, savetif=False, buffer_width=5)
+        visualise_categories_sidebyside(
+            ds1['buffer_categories'], ds2['buffer_categories'],
+            colormap=buffer_categories_cmap, labels=buffer_categories_labels,
+            title1="buffer_width=1", title2="buffer_width=5"
+        )
+        
+        # gullies+roads vs gullies+roads+ridges
+        ds_gul_roads = buffer_categories(cover_file, gullies_file, roads_data=roads_file, outdir='/tmp', stub='gul_roads', plot=False, savetif=False, buffer_width=3)
+        ds_all = buffer_categories(cover_file, gullies_file, ridges_data=ridges_file, roads_data=roads_file, outdir='/tmp', stub='gul_rid_roads', plot=False, savetif=False, buffer_width=3)
+        visualise_categories_sidebyside(
+            ds_gul_roads['buffer_categories'], ds_all['buffer_categories'],
+            colormap=buffer_categories_cmap, labels=buffer_categories_labels,
+            title1="Gullies + Roads", title2="Gullies + Roads + Ridges"
+        )
     """
     if isinstance(cover_data, str):
         da_cover = rxr.open_rasterio(cover_data).squeeze('band').drop_vars('band')
